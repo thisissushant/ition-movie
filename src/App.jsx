1d3c160befd41c028ca806f25b26aa63141878b5
Sushant Singh <sushantbibhu@gmail.com>
2024-04-30 09:07:48 +0530

    initial setup completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {} from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 font-bold ">This is App Page</h1>
+    </>
+  );
+}
+
+export default App;
